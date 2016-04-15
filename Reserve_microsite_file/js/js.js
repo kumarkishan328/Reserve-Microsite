77f02b25b649bc34a97e0331da5ab94be031746c
kumarkishan328 <kumarkishan328@gmail.com>
2016-04-15 16:17:12 +0530

    Discover your passions for finer things in life.

---
+++ Reserve_microsite_file/js/js.js
@@ -0,0 +1,76 @@
+$(function() {
+$( "#accordion" ).accordion({
+heightStyle: "content",
+collapsible: true,
+active: false,
+hide: true
+});
+
+ $(document).ready(function() {
+        $("#accordion1").accordion({
+            collapsible: true,
+            active: false,
+            clearStyle: true
+        });
+      });
+
+$( "#accordion1" ).accordion({
+heightStyle: "content",
+collapsible: true,
+active: false,
+hide: true
+});
+
+$( "#accordion2" ).accordion({
+heightStyle: "content",
+collapsible: true,
+active: false,
+hide: true
+});
+
+$( "#accordion3" ).accordion({
+heightStyle: "content",
+collapsible: true,
+active: false,
+hide: true
+});
+
+$( "#accordion4" ).accordion({
+heightStyle: "content",
+collapsible: true,
+active: false,
+hide: true
+});
+
+$( "#accordion5" ).accordion({
+heightStyle: "content",
+collapsible: true,
+active: false,
+hide: true
+});
+
+
+$( "#accordion6" ).accordion({
+heightStyle: "content",
+collapsible: true,
+active: false,
+hide: true
+});
+
+$( "#accordion7" ).accordion({
+heightStyle: "content",
+collapsible: true,
+active: false,
+hide: true
+});
+
+$( "#accordion8" ).accordion({
+heightStyle: "content",
+collapsible: true,
+active: false,
+hide: true
+});
+
+});
+
+
